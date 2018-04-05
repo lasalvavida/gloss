3946eb82b66e0341efdc83f099f4b7d696bf2862
Rob Taglang <rob@privatemachines.com>
2018-04-04 20:25:33 -0400

    Move all implementation into cpp

--- include/gloss/gloss.h
+++ include/gloss/gloss.h
@@ -14,12 +14,12 @@ namespace gloss {
 
 	std::string style(std::string str, Style textStyle);
 
-	std::string bold(std::string str) { return style(str, Style::BOLD); }
-	std::string dim(std::string str) { return style(str, Style::DIM); }
-	std::string underline(std::string str) { return style(str, Style::UNDERLINE); }
-	std::string blink(std::string str) { return style(str, Style::BLINK); }
-	std::string reverse(std::string str) { return style(str, Style::REVERSE); }
-	std::string hidden(std::string str) { return style(str, Style::HIDDEN); }
+	std::string bold(std::string str);
+	std::string dim(std::string str);
+	std::string underline(std::string str);
+	std::string blink(std::string str);
+	std::string reverse(std::string str);
+	std::string hidden(std::string str);
 
 	enum Color {
 		BLACK = 0,
@@ -42,39 +42,39 @@ namespace gloss {
 
 	std::string color(std::string str, Color textColor);
 
-	std::string black(std::string str) { return color(str, Color::BLACK); }
-	std::string red(std::string str) { return color(str, Color::RED); }
-	std::string green(std::string str) { return color(str, Color::GREEN); }
-	std::string yellow(std::string str) { return color(str, Color::YELLOW); }
-	std::string blue(std::string str) { return color(str, Color::BLUE); }
-	std::string magenta(std::string str) { return color(str, Color::MAGENTA); }
-	std::string cyan(std::string str) { return color(str, Color::CYAN); }
-	std::string lightGray(std::string str) { return color(str, Color::LIGHT_GRAY); }
-	std::string darkGray(std::string str) { return color(str, Color::DARK_GRAY); }
-	std::string lightRed(std::string str) { return color(str, Color::LIGHT_RED); }
-	std::string lightGreen(std::string str) { return color(str, Color::LIGHT_GREEN); }
-	std::string lightYellow(std::string str) { return color(str, Color::LIGHT_YELLOW); }
-	std::string lightBlue(std::string str) { return color(str, Color::LIGHT_BLUE); }
-	std::string lightMagenta(std::string str) { return color(str, Color::LIGHT_MAGENTA); }
-	std::string lightCyan(std::string str) { return color(str, Color::LIGHT_CYAN); }
-	std::string white(std::string str) { return color(str, Color::WHITE); }
+	std::string black(std::string str);
+	std::string red(std::string str);
+	std::string green(std::string str);
+	std::string yellow(std::string str);
+	std::string blue(std::string str);
+	std::string magenta(std::string str);
+	std::string cyan(std::string str);
+	std::string lightGray(std::string str);
+	std::string darkGray(std::string str);
+	std::string lightRed(std::string str);
+	std::string lightGreen(std::string str);
+	std::string lightYellow(std::string str);
+	std::string lightBlue(std::string str);
+	std::string lightMagenta(std::string str);
+	std::string lightCyan(std::string str);
+	std::string white(std::string str);
 
 	std::string fillColor(std::string str, Color backgroundColor);
 
-	std::string fillBlack(std::string str) { return fillColor(str, Color::BLACK); }
-	std::string fillRed(std::string str) { return fillColor(str, Color::RED); }
-	std::string fillGreen(std::string str) { return fillColor(str, Color::GREEN); }
-	std::string fillYellow(std::string str) { return fillColor(str, Color::YELLOW); }
-	std::string fillBlue(std::string str) { return fillColor(str, Color::BLUE); }
-	std::string fillMagenta(std::string str) { return fillColor(str, Color::MAGENTA); }
-	std::string fillCyan(std::string str) { return fillColor(str, Color::CYAN); }
-	std::string fillLightGray(std::string str) { return fillColor(str, Color::LIGHT_GRAY); }
-	std::string fillDarkGray(std::string str) { return fillColor(str, Color::DARK_GRAY); }
-	std::string fillLightRed(std::string str) { return fillColor(str, Color::LIGHT_RED); }
-	std::string fillLightGreen(std::string str) { return fillColor(str, Color::LIGHT_GREEN); }
-	std::string fillLightYellow(std::string str) { return fillColor(str, Color::LIGHT_YELLOW); }
-	std::string fillLightBlue(std::string str) { return fillColor(str, Color::LIGHT_BLUE); }
-	std::string fillLightMagenta(std::string str) { return fillColor(str, Color::LIGHT_MAGENTA); }
-	std::string fillLightCyan(std::string str) { return fillColor(str, Color::LIGHT_CYAN); }
-	std::string fillWhite(std::string str) { return fillColor(str, Color::WHITE); }
+	std::string fillBlack(std::string str);
+	std::string fillRed(std::string str);
+	std::string fillGreen(std::string str);
+	std::string fillYellow(std::string str);
+	std::string fillBlue(std::string str);
+	std::string fillMagenta(std::string str);
+	std::string fillCyan(std::string str);
+	std::string fillLightGray(std::string str);
+	std::string fillDarkGray(std::string str);
+	std::string fillLightRed(std::string str);
+	std::string fillLightGreen(std::string str);
+	std::string fillLightYellow(std::string str);
+	std::string fillLightBlue(std::string str);
+	std::string fillLightMagenta(std::string str);
+	std::string fillLightCyan(std::string str);
+	std::string fillWhite(std::string str);
 }
--- src/gloss/gloss.cpp
+++ src/gloss/gloss.cpp
@@ -8,12 +8,129 @@ string gloss::style(string str, Style textStyle) {
 		str + "\e[" + to_string(20 + textStyle) + "m";
 }
 
+string gloss::bold(string str) { 
+	return style(str, Style::BOLD); 
+}
+string gloss::dim(string str) { 
+	return style(str, Style::DIM); 
+}
+string gloss::underline(string str) { 
+	return style(str, Style::UNDERLINE); 
+}
+string gloss::blink(string str) {
+	return style(str, Style::BLINK);
+}
+string gloss::reverse(string str) {
+	return style(str, Style::REVERSE);
+}
+string gloss::hidden(string str) {
+	return style(str, Style::HIDDEN); 
+}
+
 string gloss::color(string str, Color textColor) {
 	return "\e[" + to_string(30 + textColor) + "m" + 
 		str + "\e[39m";
 }
 
+string gloss::black(string str) {
+	return color(str, Color::BLACK);
+}
+string gloss::red(string str) {
+	return color(str, Color::RED);
+}
+string gloss::green(string str) {
+	return color(str, Color::GREEN);
+}
+string gloss::yellow(string str) {
+	return color(str, Color::YELLOW);
+}
+string gloss::blue(string str) {
+	return color(str, Color::BLUE);
+}
+string gloss::magenta(string str) {
+	return color(str, Color::MAGENTA);
+}
+string gloss::cyan(string str) {
+	return color(str, Color::CYAN);
+}
+string gloss::lightGray(string str) {
+	return color(str, Color::LIGHT_GRAY);
+}
+string gloss::darkGray(string str) {
+	return color(str, Color::DARK_GRAY);
+}
+string gloss::lightRed(string str) {
+	return color(str, Color::LIGHT_RED);
+}
+string gloss::lightGreen(string str) {
+	return color(str, Color::LIGHT_GREEN);
+}
+string gloss::lightYellow(string str) {
+	return color(str, Color::LIGHT_YELLOW);
+}
+string gloss::lightBlue(string str) {
+	return color(str, Color::LIGHT_BLUE);
+}
+string gloss::lightMagenta(string str) {
+	return color(str, Color::LIGHT_MAGENTA);
+}
+string gloss::lightCyan(string str) {
+	return color(str, Color::LIGHT_CYAN);
+}
+string gloss::white(string str) {
+	return color(str, Color::WHITE);
+}
+
 string gloss::fillColor(string str, Color backgroundColor) {
 	return "\e[" + to_string(40 + backgroundColor) + "m" + 
 		str + "\e[49m";
+}
+
+string gloss::fillBlack(string str) {
+	return fillColor(str, Color::BLACK);
+}
+string gloss::fillRed(string str) {
+	return fillColor(str, Color::RED);
+}
+string gloss::fillGreen(string str) {
+	return fillColor(str, Color::GREEN);
+}
+string gloss::fillYellow(string str) {
+	return fillColor(str, Color::YELLOW);
+}
+string gloss::fillBlue(string str) {
+	return fillColor(str, Color::BLUE);
+}
+string gloss::fillMagenta(string str) {
+	return fillColor(str, Color::MAGENTA);
+}
+string gloss::fillCyan(string str) {
+	return fillColor(str, Color::CYAN);
+}
+string gloss::fillLightGray(string str) {
+	return fillColor(str, Color::LIGHT_GRAY);
+}
+string gloss::fillDarkGray(string str) {
+	return fillColor(str, Color::DARK_GRAY);
+}
+string gloss::fillLightRed(string str) {
+	return fillColor(str, Color::LIGHT_RED);
+}
+string gloss::fillLightGreen(string str) {
+	return fillColor(str, Color::LIGHT_GREEN);
+}
+string gloss::fillLightYellow(string str) {
+	return fillColor(str, Color::LIGHT_YELLOW);
+}
+string gloss::fillLightBlue(string str) {
+	return fillColor(str, Color::LIGHT_BLUE);
+}
+string gloss::fillLightMagenta(string str) {
+	return fillColor(str, Color::LIGHT_MAGENTA);
+}
+string gloss::fillLightCyan(string str) {
+	return fillColor(str, Color::LIGHT_CYAN);
+}
+string gloss::fillWhite(string str) {
+	return fillColor(str, Color::WHITE);
 }
